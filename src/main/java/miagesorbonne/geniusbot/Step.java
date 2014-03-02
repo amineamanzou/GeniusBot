9e587e3e6ad61d54f59ee08573e08528f5a02968
Amine Amanzou <amine@lacompany.fr>
2014-03-02 23:10:06 +0100

    Adding automate state class manager
    Updating config structure.

---
+++ src/main/java/miagesorbonne/geniusbot/Step.java
@@ -0,0 +1,80 @@
+package miagesorbonne.geniusbot;
+
+import java.util.ArrayList;
+
+/**
+ * A step is a state where the automate will be 
+ * depending on configs. 
+ * It's a situation or a part of a situation 
+ * (use case)
+ * 
+ * @author Amine Amanzou <amineamanzou@gmail.com>
+ */
+public class Step {
+    
+    private String id = "";
+    private ArrayList keywords;
+    private ArrayList<String> messages;
+    
+    /**
+     * Default Constructor
+     * 
+     * @param id
+     * @param keywords
+     * @param messages 
+     */
+    public Step(String id, ArrayList keywords, ArrayList messages){
+        this.id = id;
+        this.keywords = keywords;
+        this.messages = messages;
+    }
+
+    /**
+     * Get the id of the step
+     * @return id
+     */
+    public String getId() {
+        return id;
+    }
+
+    /**
+     * Set the id
+     * @param id 
+     */
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    /**
+     * Get the keywords to match in this step
+     * @return 
+     */
+    public ArrayList getKeywords() {
+        return keywords;
+    }
+
+    /**
+     * Set the keywords to match in this step
+     * @param keywords 
+     */
+    public void setKeywords(ArrayList keywords) {
+        this.keywords = keywords;
+    }
+
+    /**
+     * Get the messages to answer in this step
+     * @return 
+     */
+    public ArrayList<String> getMessages() {
+        return messages;
+    }
+
+    /**
+     * Set the messages to answer in this step
+     * @param messages 
+     */
+    public void setMessages(ArrayList<String> messages) {
+        this.messages = messages;
+    }
+    
+}
