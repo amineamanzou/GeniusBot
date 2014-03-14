3d6086992cf9cffe3f6f83829eafd5ffc2f2e887
Amine Amanzou <amine@lacompany.fr>
2014-03-14 23:38:32 +0100

    Finishing Weather plugin
    @TODO translate the result obtained by yahoo API according to the code
    sent with the information

--- src/main/java/miagesorbonne/geniusbot/plugins/MeteoPlugin.java
+++ src/main/java/miagesorbonne/geniusbot/plugins/MeteoPlugin.java
@@ -34,9 +34,71 @@ public class MeteoPlugin {
      * Main constructor
      */
     public MeteoPlugin() {
+        HttpClient client = new HttpClient();
+        GetMethod method = new GetMethod(url);
+
+        try {
+            // Executing the GET query
+            int statusCode = client.executeMethod(method);
+
+            // If method execution failed
+            if (statusCode != HttpStatus.SC_OK) {
+                System.err.println("Method failed: " + method.getStatusLine());
+            }
+            
+            BufferedReader br = new BufferedReader(new InputStreamReader(method.getResponseBodyAsStream()));
+
+            String xmlResponse = "";
+            String readLine;
+            while (((readLine = br.readLine()) != null)) {
+                xmlResponse += readLine;
+            }
+
+            // Parse the xml
+            parse(xmlResponse);
+        } catch (HttpException e) {
+            System.err.println("Violation de protocol: " + e.getMessage());
+            e.printStackTrace();
+        } catch (IOException e) {
+            System.err.println("Erreur de transport: " + e.getMessage());
+            e.printStackTrace();
+        } finally {
+            // Release the connection.
+            method.releaseConnection();
+        }
 
     }
 
+    /**
+     * Parsing the XML sent by the API
+     * @param xml 
+     */
+    private void parse(String xml) {
+        DocumentBuilderFactory dbf =
+                DocumentBuilderFactory.newInstance();
+        try {
+            DocumentBuilder db = dbf.newDocumentBuilder();
+            InputSource is = new InputSource();
+            is.setCharacterStream(new StringReader(xml));
+
+            // Parsing dom
+            Document dom = db.parse(is);
+
+            // Getting current day node
+            NodeList nodes = dom.getElementsByTagName("yweather:condition");
+
+            // Storing Nod of tomorow and after tomorrow
+            NodeList forcast = dom.getElementsByTagName("yweather:forecast");
+
+            // Get now, tomorrow and day after tomorrow conditions from nodes
+            now = ((Element) nodes.item(0)).getAttribute("text");
+            tomorrow = ((Element) forcast.item(0)).getAttribute("text");
+            dayAfterTomorrow = ((Element) forcast.item(1)).getAttribute("text");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+    
     /**
      * Return the answer about weather
      * @param condition
@@ -47,17 +109,17 @@ public class MeteoPlugin {
 
         // If argument is now or empty, return now's weather
         if (condition.equals("now") || condition.length() == 0) {
-            weather = "Aujourd'hui il fait : " + now + " (Resultat fournis en anglais par Yahoo) ";
+            weather = "Aujourd'hui il fait : " + now + " à Paris (Resultat fournis en anglais par Yahoo) ";
         }
 
         // Condition : demain weather information
         if (condition.equals("demain")) {
-            weather = "Je suppose que demain il fera : " + tomorrow + " (Resultat fournis en anglais par Yahoo) ";
+            weather = "Je suppose que demain il fera : " + tomorrow + " à Paris (Resultat fournis en anglais par Yahoo) ";
         }
 
         // Condition : apres demain weather information
         if (condition.equals("apresdemain")) {
-            weather = "Après demain il fera : " + dayAfterTomorrow + " (Resultat fournis en anglais par Yahoo) ";
+            weather = "Après demain il fera : " + dayAfterTomorrow + " à Paris (Resultat fournis en anglais par Yahoo) ";
         }
         return weather;
     }
