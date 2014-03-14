fd3b1e19f76c2a12af1453a78b56added60b5cca
Amine Amanzou <amine@lacompany.fr>
2014-03-14 23:10:59 +0100

    Adding a new weather plugin
    Adding dependencies to maven to use Yahoo API

---
+++ src/main/java/miagesorbonne/geniusbot/plugins/MeteoPlugin.java
@@ -0,0 +1,15 @@
+package miagesorbonne.geniusbot.plugins;
+
+
+/**
+ * Weather Plugin that add weather info functionnality to the bot
+ *
+ * @author Alexandre Le Boucher <alex.leboucher.inef@gmail.com>
+ * @author Shinthujan Sivakumar <shinthujan.sivakumar@gmail.com>
+ * @author Amine Amanzou <amineamanzou@gmail.com>
+ */
+public class MeteoPlugin {
+    
+    
+
+}
